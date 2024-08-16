89de8d6d15fd2ad7d77278eafb4c2fe432b4c239
knight <karvin_jimenez@hotmail.com>
2024-08-16 10:01:28 -0400

    project started.

---
+++ source/template.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiiuse/wpad.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+SDL_GameController *controller = nullptr;
+
+const int SPEED = 600;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Rect player = {SCREEN_WIDTH / 2 - 64, SCREEN_HEIGHT / 2 - 64, 64, 64};
+
+void quitGame()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void handleEvents()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            quitGame();
+            exit(0);
+        }
+    }
+}
+
+void update(float deltaTime)
+{
+
+    // WPAD_ButtonsDown tells us which buttons were pressed in this loop
+    // this is a "one shot" state which will not fire again until the button has been released
+    const u32 padDown = WPAD_ButtonsDown(0);
+
+    // WPAD_ButtonsHeld tells us which buttons are keep pressing in this loop
+    const u32 padHeld = WPAD_ButtonsHeld(0);
+
+    // We return to the launcher application via exit
+    if (padDown & WPAD_BUTTON_HOME)
+        exit(0);
+
+    if (padHeld & WPAD_BUTTON_LEFT && player.x > 0)
+    {
+        player.x -= SPEED * deltaTime;
+    }
+
+    else if (padHeld & WPAD_BUTTON_RIGHT && player.x < SCREEN_WIDTH - player.w)
+    {
+        player.x += SPEED * deltaTime;
+    }
+
+    else if (padHeld & WPAD_BUTTON_UP && player.y > 0)
+    {
+        player.y -= SPEED * deltaTime;
+    }
+    
+    else if (padHeld & WPAD_BUTTON_DOWN && player.y < SCREEN_HEIGHT - player.h)
+    {
+        player.y += SPEED * deltaTime;
+    }
+}
+
+void render()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    SDL_RenderFillRect(renderer, &player);
+
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char **argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        SDL_Log("SDL_Init: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        SDL_Log("SDL_CreateWindow: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer)
+    {
+        SDL_Log("SDL_CreateRenderer: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    WPAD_Init();
+
+    Uint32 previousFrameTime = SDL_GetTicks();
+    Uint32 currentFrameTime = previousFrameTime;
+    float deltaTime = 0.0f;
+
+    while (true)
+    {
+        // Call WPAD_ScanPads each loop, this reads the latest controller states
+        WPAD_ScanPads();
+
+        currentFrameTime = SDL_GetTicks();
+        deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f; // Convert to seconds
+        previousFrameTime = currentFrameTime;
+
+        handleEvents();
+        update(deltaTime);
+        render();
+    }
+
+    quitGame();
+}
